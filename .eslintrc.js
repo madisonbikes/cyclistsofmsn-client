def2b161b84f52a8ea933e67baf5455a097a54c9
Ben Sandee <tbsandee@orangebikelabs.com>
2022-06-04 12:43:22 -0500

    enable eqeqeq lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,10 @@ module.exports = {
     ecmaVersion: 6,
   },
   plugins: ["react", "@typescript-eslint", "no-autofix"],
+  reportUnusedDisableDirectives: true,
   rules: {
     "prefer-const": "off",
     "no-autofix/prefer-const": "error",
+    eqeqeq: ["warn", "smart"],
   },
 };
