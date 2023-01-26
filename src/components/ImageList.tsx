3e7d86d106113406a9f556036e5c76510ab56379
Ben Sandee <tbsandee@orangebikelabs.com>
2023-01-25 18:57:12 -0600

    feat: add image detail and good imagelist components

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,27 +1,77 @@
 import { loadImageList } from "../api/images";
-import { ListItem } from "@mui/material";
+import { IconButton, Link } from "@mui/material";
 import { useQuery } from "react-query";
 import { Image } from "../api/contract";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { Edit } from "@mui/icons-material";
+import { useNavigate, Link as RouterLink } from "react-router-dom";
+import { GridInitialStateCommunity } from "@mui/x-data-grid/models/gridStateCommunity";
+import { RawImage } from "./RawImage";
 
 export const ImageList = () => {
   const { data, isLoading, isError, error } = useQuery<Image[], Error>({
     queryKey: "imageList",
     queryFn: () => loadImageList(),
   });
-  if (isLoading) return <>Loading...</>;
-  if (isError) return <>An error has occurred {error.message}</>;
+  const navigate = useNavigate();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>An error has occurred {error.message}</div>;
+
+  const onModifyClicked = (id: string) => {
+    navigate(`/images/${id}`);
+  };
+
+  if (!data) {
+    throw new Error("data should always exist");
+  }
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 240 },
+    {
+      field: "image",
+      headerName: "Image",
+      width: 108,
+      renderCell: (params: GridRenderCellParams<Image>) => (
+        <Link component={RouterLink} to={`/images/${params.id}`}>
+          <RawImage id={String(params.id)} width={96} />
+        </Link>
+      ),
+    },
+    { field: "filename", headerName: "Filename", width: 200 },
+    { field: "description", headerName: "Description", width: 400 },
+    {
+      field: "buttons",
+      headerName: "Ops",
+      renderCell: (params: GridRenderCellParams<Image>) => (
+        <>
+          <IconButton onClick={() => onModifyClicked(params.row.id)}>
+            <Edit />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
+  const initialState: GridInitialStateCommunity = {
+    columns: { columnVisibilityModel: { id: false } },
+  };
+
   return (
-    <ul>
-      {data?.map((image) => {
-        return (
-          <ListItem key={image.id}>
-            <>
-              <img src={`/api/v1/images/${image.id}?width=96`} alt="cyclist" />
-              {image.description}
-            </>
-          </ListItem>
-        );
-      })}
-    </ul>
+    <>
+      <h2>Images</h2>
+      <div
+        style={{
+          width: "100%",
+          height: 800,
+        }}
+      >
+        <DataGrid
+          rows={data}
+          columns={columns}
+          getRowHeight={() => "auto"}
+          initialState={initialState}
+        />
+      </div>
+    </>
   );
 };
