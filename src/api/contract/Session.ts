4183d9132c5406cb5cc9414bfdbdc05b5a101eaf
Ben Sandee <tbsandee@orangebikelabs.com>
2023-01-27 22:20:22 -0600

    refactor: use common prefix for session endpoints

--- src/api/contract/Session.ts
+++ src/api/contract/Session.ts
@@ -1,7 +1,9 @@
 import { Request, post, get } from "superagent";
 
+const prefix = `/api/v1/session`;
+
 export const Session = {
-  login: (): Request => post(`/api/v1/session/login`),
-  logout: (): Request => post(`/api/v1/session/logout`),
-  info: (): Request => get(`/api/v1/session/info`),
+  login: (): Request => post(`${prefix}/login`),
+  logout: (): Request => post(`${prefix}/logout`),
+  info: (): Request => get(`${prefix}/info`),
 };
