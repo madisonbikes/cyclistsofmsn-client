7511cdaefbe6f55f1294abdd328835115962351f
Ben Sandee <tbsandee@orangebikelabs.com>
2023-04-12 07:54:13 -0500

    fix: updated types, error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,27 @@ import { Login } from "./components/forms/Login";
 import { Current } from "./components/Current";
 import { ImageList } from "./components/ImageList";
 import { ImageDetail } from "./components/ImageDetail";
-import { ErrorBoundary } from "react-error-boundary";
-import { ErrorFallback } from "./common";
 import { PostDetail } from "./components/PostDetail";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route index element={<Current />} />
-            <Route path="posts" element={<Outlet />}>
-              <Route index element={<PostList />} />
-              <Route path=":id" element={<PostDetail />} />
-            </Route>
-            <Route path="images" element={<Outlet />}>
-              <Route index element={<ImageList />} />
-              <Route path=":id" element={<ImageDetail />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="info" element={<Info />} />
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Current />} />
+          <Route path="posts" element={<Outlet />}>
+            <Route index element={<PostList />} />
+            <Route path=":id" element={<PostDetail />} />
           </Route>
-        </Routes>
-      </ErrorBoundary>
+          <Route path="images" element={<Outlet />}>
+            <Route index element={<ImageList />} />
+            <Route path=":id" element={<ImageDetail />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="logout" element={<Logout />} />
+          <Route path="info" element={<Info />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { AuthProvider, theme } from "./common";
+import { AuthProvider, theme, ErrorFallback } from "./common";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,12 +27,14 @@ root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ErrorBoundary>
     </React.StrictMode>
   </ThemeProvider>
 );
